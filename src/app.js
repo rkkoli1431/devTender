62132d5c4cd87a6081365d3fcb92b473519d891f
Rahul <rahulkoli9320@gmail.com>
2025-05-06 18:01:16 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,30 @@
 const express = require("express");
 
 const app = express();
-
+//  this is rejex important =>  /.*fly/ 
 //This will only handle GET call to / user
-app.get("/user",(req, res)=>{
+app.get("/user/:userId/:name/:password",(req, res)=>{
+    console.log(req.params);     
     res.send({firstName: "Rahul", lastName: "Koli"})
 });
 
-app.post("/user",(req, res)=>{
-    res.send("Save Data Successfully On Database");
-});
 
-app.delete("/user",(req, res)=>{
-    res.send("Data Deleted Successfully !");
-});
+// app.post("/user",(req, res)=>{
+//     res.send("Save Data Successfully On Database");
+// });
 
-// this will match all the HTTP method API calls to /test
-app.use("/get", (req, res)=>{
-    res.send("Get the Hello hello hello !!!!!!");
-});
+// app.delete("/user",(req, res)=>{
+//     res.send("Data Deleted Successfully !");
+// });
+
+// // this will match all the HTTP method API calls to /test
+// app.use("/get", (req, res)=>{
+//     res.send("Get the Hello hello hello !!!!!!");
+// });
 
 
 app.listen(8000, ()=>{
-    console.log("Server is successfully listening on port 3000.....");
+    console.log("Server is successfully listening on port 8000.....");
 });
 
 
