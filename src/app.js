332d1d37e2564255cebac98b7588b150aa8a7972
Rahul <rahulkoli9320@gmail.com>
2025-05-09 12:00:01 +0530

    route-handler-middlewars-practice

--- src/app.js
+++ src/app.js
@@ -1,41 +1,30 @@
 const express = require("express");
 
 const app = express();
-//  this is rejex important =>  /.*fly/ 
-//This will only handle GET call to / user
-app.get("/user/:userId/:name/:password",(req, res)=>{
-    console.log(req.params);     
-    res.send({firstName: "Rahul", lastName: "Koli"})
+
+
+app.use("/user", (req, res, next)=>{
+    console.log("Handaling route user 1 !!");
+    // res.send("1st Responce ");
+    next();
+    
+},
+(req, res, next) =>{
+    console.log("Handaling route user 2 !!");
+   // res.send("2nd Responce")
+    next();
+},
+(req, res, next)=>{
+    console.log("Handaling route user 3 !!");
+    // res.send("3rd Responce");
+    next();
+    
+},(req, res,next)=>{
+    console.log("Handaling route user 4 !!")
+    // res.send("4th Responce");
+    next();
 });
-
-
-// app.post("/user",(req, res)=>{
-//     res.send("Save Data Successfully On Database");
-// });
-
-// app.delete("/user",(req, res)=>{
-//     res.send("Data Deleted Successfully !");
-// });
-
-// // this will match all the HTTP method API calls to /test
-// app.use("/get", (req, res)=>{
-//     res.send("Get the Hello hello hello !!!!!!");
-// });
-
 
 app.listen(8000, ()=>{
-    console.log("Server is successfully listening on port 8000.....");
+    console.log("Server is successfully run on port 8000...");
 });
-
-
-
-
-// app.use("/set", (req, res)=>{
-//     res.send("Hello World !!!!!!");
-// });
-
-
-// app.use("/", (req, res )=>{
-//     res.send("THIS IS MY CODECHAMP'S DASHBOARD");
-//     console.log("Hello");
-// });
