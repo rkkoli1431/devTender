41b441f6f3f9f9ca4c81ad3edf3b1a971ecc6931
Rahul <rahulkoli9320@gmail.com>
2025-05-09 18:01:01 +0530

    writting-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,29 +1,24 @@
 const express = require("express");
 
 const app = express();
+const {adminAuth, userAuth} = require("./middlewares/auth");
 
 
-app.use("/user", (req, res, next)=>{
-    console.log("Handaling route user 1 !!");
-    // res.send("1st Responce ");
-    next();
-    
-},
-(req, res, next) =>{
-    console.log("Handaling route user 2 !!");
-   // res.send("2nd Responce")
-    next();
-},
-(req, res, next)=>{
-    console.log("Handaling route user 3 !!");
-    // res.send("3rd Responce");
-    next();
-    
-},(req, res,next)=>{
-    console.log("Handaling route user 4 !!")
-    // res.send("4th Responce");
-    next();
+app.use("/admin",adminAuth);
+// app.use("/user",userAuth);       also write like this middlewares
+
+app.get("/admin/getData", (req, res)=>{
+    res.send("All Data Sent !!!");
 });
+
+app.get("/user",userAuth, (req, res)=>{
+    res.send("User Data Sent ...");
+})
+
+app.get("/admin/deleteData", (req, res)=>{
+    res.send("Deleted the user ....");
+});
+
 
 app.listen(8000, ()=>{
     console.log("Server is successfully run on port 8000...");
