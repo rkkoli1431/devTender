b7a11ad9e1598c9f7b09f3c255c1d9b2d25add0a
Rahul <rahulkoli9320@gmail.com>
2025-05-10 09:54:09 +0530

    wildcard-error-handaling

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
 const express = require("express");
 
 const app = express();
-const {adminAuth, userAuth} = require("./middlewares/auth");
 
 
-app.use("/admin",adminAuth);
-// app.use("/user",userAuth);       also write like this middlewares
-
-app.get("/admin/getData", (req, res)=>{
-    res.send("All Data Sent !!!!");
+app.get("/getUserData",(req, res)=>{
+    try{
+        // Logic of DB and get the user Data
+        throw new Error("egfjgfrjfgrj");
+        res.send("User Data Sent....")
+    }
+    catch(err){
+        res.status(500).send("Something went wrong 1 !!!!");
+    }
 });
 
-app.get("/user",userAuth, (req, res)=>{
-    res.send("User Data Sent ...");
-})
-
-app.get("/admin/deleteData", (req, res)=>{
-    res.send("Deleted the user ....");
+app.use("/",(err, req, res, next)=>{            // in error handaling always use in last because order matter err variable keep it last 
+    console.log("Something went wrong !!!");
+    if(err){
+        res.status(500).send("Something went wrong !!!");
+    }
 });
+
 
 
 app.listen(8000, ()=>{
