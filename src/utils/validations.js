c10018b7f815f0054223edc476ac6e467c40b2aa
Rahul <rahulkoli9320@gmail.com>
2025-06-19 14:57:11 +0530

    validation-edit-profile-Data

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -12,6 +12,24 @@ const validateSignUpData = (req)=>{
     }
 }
 
+const validateEditProfileDate = (req) =>{
+    const allowedEditFields = [
+        "firstName",
+        "lastName",
+        "emailId",
+        "photoUrl",
+        "gender",
+        "age",
+        "about",
+        "skills",
+    ];
+
+    const isEditAllowed = Object.keys(req.body).every((field) => allowedEditFields.includes(field));
+    return isEditAllowed;
+}   
+
+
 module.exports = {
     validateSignUpData,
+    validateEditProfileDate,
 }; 
