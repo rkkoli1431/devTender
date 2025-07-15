47aab5def1d98a48085aec5805c55b8e98535e0a
Rahul <rahulkoli9320@gmail.com>
2025-07-15 22:14:10 +0530

    pre scheama connection

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -23,6 +23,16 @@ const connectionRequestSchema = new mongoose.Schema({
 { timestamps: true,}
 );
 
+connectionRequestSchema.pre("save", function (next){
+    const connectionRequest = this;
+    // Check if the fromUserId  is same as toUserId
+
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send connection request to yourself ! ");
+    }
+    next();
+})
+
 const ConnectionRequestModel = new mongoose.model(
     "ConnectionRequest",
     connectionRequestSchema
