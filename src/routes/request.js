58132a100912a99b20a8cc7e41bcb36014ae8d42
Rahul <rahulkoli9320@gmail.com>
2025-07-15 22:14:31 +0530

    massage chages

--- src/routes/request.js
+++ src/routes/request.js
@@ -46,7 +46,7 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth, async (req, res)=
         
         const data = await connectionRequest.save();
         res.json({
-            message: "Connection Request Sent Successfully !",
+            message: req.user.firstName + " is " + status + " in " + toUser.firstName,
             data,
         });
     }catch(err){
