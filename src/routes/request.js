c5519218006fe743c5fb4bd3a1f459dc23abbd45
Rahul <rahulkoli9320@gmail.com>
2025-07-16 18:49:57 +0530

    review status API

--- src/routes/request.js
+++ src/routes/request.js
@@ -53,6 +53,38 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth, async (req, res)=
         res.status(400).send("ERROR: " +err.message);
     }
     // res.send(user.firstName +" Connection Request Sent !!");
+});
+
+requestRouter.post("/request/review/status/:requestId",userAuth, async (req, res) => {
+    try{
+        const loggedInuser = req.user;
+        const {status, requestId} = req.params;
+
+        const allowedStatus = ["accepeted", "rejected"];
+
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({ massage: "Status not allowed !"})
+        }
+
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInuser._id,
+            status: "interested",
+        });
+
+        if(!connectionRequest){
+            return res.status(404).json({massage: "Connection Request Not Found"});
+        }
+
+        connectionRequest.status = status;
+
+        const data = await connectionRequest.save();
+
+        res.json({massage: "Connection request " +status, data }); 
+
+    }catch(err){
+        res.status(400).send("ERROR : "+err.massage);
+    }
 })
 
 module.exports = requestRouter;
