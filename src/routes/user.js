f0a32740d0bb9d7650680efe98d97c1ceb022d9b
Rahul <rahulkoli9320@gmail.com>
2025-08-05 23:43:17 +0530

    normal changes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
+const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
 userRouter.get("/user/requests/received",userAuth, async(req, res)=>{
-
     try{
-        const loggedInuser = req.user;
+        const loggedInUser = req.user;
 
-        const connectionRequest = await connectionRequest.find({
-            toUserId: loggedInuser._id,
-            status: "interested",
+        const connectionRequest = await ConnectionRequest.find({
+            toUserId: loggedInUser ._id,
         });
 
         res.json({
-            message: "Data fetched successfully ",
+            message: "Data fetch successfully",
             data: connectionRequest,
-        });
+        })
     }catch(err){
-        res.status(400).send("ERROR: " +err.message);
+        res.status(400).send("ERROR :" +err.message);
     }
 })
+
+module.exports = userRouter;
 
