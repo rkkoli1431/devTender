22e5729021386ac85085b60c0cebc7ec4cf54a52
Rahul <rahulkoli9320@gmail.com>
2025-08-26 23:12:23 +0530

    add ref in connectionRequest Scheama & check user itself connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const USER_SAFE_DATA = ["firstName", "lastName"];
 
 userRouter.get("/user/requests/received",userAuth, async(req, res)=>{
     try{
@@ -28,11 +29,18 @@ userRouter.get("/user/connections", userAuth, async (req, res)=>{
         const connectionRequest = await ConnectionRequest.find({
             $or: [
                 {toUserId: loggedInUser._id, status: "accepeted" },
-                {fromUseId: loggedInUser._id, status: "accepeted" },
+                {fromUserId: loggedInUser._id, status: "accepeted" },
             ],
-        }).populate("fromUserId", ["firstName", "lastName"]);
+        })
+        .populate("fromUserId",USER_SAFE_DATA )
+        .populate("toUserId", USER_SAFE_DATA);
 
-        const data = connectionRequest.map((row) => row.fromUserId);
+        const data = connectionRequest.map((row) => {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
         res.json({data});
     }catch(err){
         res.status(400).send({massage: err.massage});
