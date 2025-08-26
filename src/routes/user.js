9ce15a5e53e0d765cc66f8dee5d2b0e0a7e1e6e6
Rahul <rahulkoli9320@gmail.com>
2025-08-26 22:40:12 +0530

    status - update in request received

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,6 +9,7 @@ userRouter.get("/user/requests/received",userAuth, async(req, res)=>{
 
         const connectionRequest = await ConnectionRequest.find({
             toUserId: loggedInUser ._id,
+            status: "interested",
         }).populate("fromUserId", ["firstName", "lastName"]);
 
         res.json({
