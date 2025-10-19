00e77fa1e166a4d4f318742c2e876cbfc955cc1e
Rahul <rahulkoli9320@gmail.com>
2025-10-19 18:38:15 +0530

    change profile update api patch to put

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -6,38 +6,38 @@ const profileRouter = express.Router();
 
 // Profile API To Get The Data
 
-profileRouter.get("/profile", userAuth, async (req, res ) => { 
-    try{ 
-        const user = req.user;
-        res.send(user);
-}catch(err){
-    res.status(400).send("ERROR : "+err.message);
-}
-});
-
-profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
-    try {
-        if(!validateEditProfileDate)
-        {
-            throw new Error("Invalid Edit Request");
-        }
-        const loggedInUser = req.user;
-        // console.log(loggedInUser);
-
-        Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
-        await loggedInUser.save();
-
-        // console.log(loggedInUser);
-
-        // res.send(`${loggedInUser.firstName},Your profile updated successfull !!`);
-        // This is the good way to sending the response
-        res.json({
-            message: `${loggedInUser.firstName},Your profile updated successfull !!`,
-            data: loggedInUser,
-        })
-    } catch(err){
-        res.status(400).send("ERROR : ", +err.message);
+    profileRouter.get("/profile", userAuth, async (req, res ) => { 
+        try{ 
+            const user = req.user;
+            res.send(user);
+    }catch(err){
+        res.status(400).send("ERROR : "+err.message);
     }
-})
+    });
 
-module.exports = profileRouter;
+    profileRouter.put("/profile/edit", userAuth, async (req, res) => {
+        try {
+            if(!validateEditProfileDate(req))
+            {
+                throw new Error("Invalid Edit Request");
+            }
+            const loggedInUser = req.user;
+            // console.log(loggedInUser);
+
+            Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+            await loggedInUser.save();
+
+            // console.log(loggedInUser);
+
+            // res.send(`${loggedInUser.firstName},Your profile updated successfull !!`);
+            // This is the good way to sending the response
+            res.json({
+                message: `${loggedInUser.firstName},Your profile updated successfull !!`,
+                data: loggedInUser,
+            })
+        } catch(err){
+            res.status(400).send("ERROR : ", +err.message);
+        }
+    })
+
+    module.exports = profileRouter;
